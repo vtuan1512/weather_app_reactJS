e22e9f610dee08764e13dbe51320b92f96af10f1
Vu Tuan <tuanva.ba11-097@st.usth.edu.vn>
2024-03-04 11:33:18 +0700

    project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchWeather from './component/SearchWeather';
+function App() {
+  return (
+    <div>
+      <SearchWeather></SearchWeather>
+    </div>
+  );
+}
+
+export default App;
